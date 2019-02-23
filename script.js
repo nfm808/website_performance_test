74b8dd42c5ffc2f3911e94b1e24e679aa4302319
Nathan Fowler <luxurysandbox@gmail.com>
2019-02-23 12:37:18 -0500

    set up organization of js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 'use strict';
+// 00 - Website Test Form
+// 01 - Page Insights API
+// 02 - Snapshot API
+// 03 - Display Elements
+// 04 - Contact Form
+
+// 00 - website test form
+function handleWebsiteForm() {
+  console.log(`handleWebsiteForm ran`);
+};
+function validateUrl() {
+  console.log(`validateUrl ran`);
+};
+// 01 - page insights api
+function createPageInsightsUrl() {
+  console.log(`createPageInsightsUrl ran`);
+};
+function getPageInsights() {
+  console.log(`getPageInsight ran`);
+};
+function createPageInsightsElements() {
+  console.log(`createPageInsightsElements ran`);
+};
+// 02 - snapshot api
+function createSnapshotUrl() {
+  console.log(`createSnapshotUrl ran`);
+};
+function getSnapshot() {
+  console.log(`getSnapshot ran`);
+};
+function createSnapshotElements() {
+  console.log(`createSnapshotElements ran`);
+};
+// 03 - display elements
+function displayElements() {
+  console.log(`displayElements ran`);
+};
+// 04 - contact form
+function validateEmail() {
+  console.log(`validateEmail ran`);
+};
+function handleContactForm() {
+  console.log(`handleContactForm ran`);
+};
